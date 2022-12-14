5a96c9125f3292dcb37acca5e1b02a8d7eb24482
neokyr <p00riel.kyrillin@gmail.com>
2022-12-14 08:39:51 -0500

    Calculatrice v1

--- paul-antoine/calculatrice/main.c
+++ paul-antoine/calculatrice/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char** argv) {
+
+    if(argc != 4) {
+        printf("Nombre d'argument incorrect 3 arguments attendus");
+        return 1;
+    }
+    float arg1 = atof(argv[1]);
+    char* operator = argv[2];
+    float arg2 = atof(argv[3]);
+
+    if(strcmp("+", operator) == 0) {
+        printf("%f + %f = %f", arg1, arg2, arg1 + arg2);
+    } else if(strcmp("*", operator) == 0) {
+        printf("%f * %f = %f", arg1, arg2, arg1 * arg2);
+    } else if(strcmp("/", operator) == 0) {
+        printf("%f / %f = %f", arg1, arg2, arg1 / arg2);
+    } else if(strcmp("-", operator) == 0) {
+        printf("%f - %f = %f", arg1, arg2, arg1 - arg2);
+    } else {
+        printf("%s n'est pas un opérateur valide", operator);
+    }
+
     return 0;
 }
