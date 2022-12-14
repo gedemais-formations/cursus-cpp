f8f075c187013f5be08e2abed7d49ea95f74c954
cedric-darrou <c.darrou@gmx.com>
2022-12-14 16:42:28 +0100

    Bonus 1
    Realization of the bonus 1 : "Make sure that you take into account 
    several factors and several operators, without respecting calculation 
    priorities."

--- cedric/main.c
+++ cedric/main.c
@@ -1,38 +1,81 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) {
-  double val1, val2, res;
-  char op;
-  //Check args
-  for (int i=1; i< argc; i++) {
-    printf("arg%d=%s\n", i, argv[i]);
-  }
-
-  //Transform args in double
-  sscanf(argv[1], "%lf", &val1);
-  op = argv[2][0];
-  sscanf(argv[3], "%lf", &val2);
-  printf("Valeur 1 : %lf\nValeur 2 : %lf\n", val1, val2);
+int calculate(int value1, int value2, char op){
+  int result;
   switch (op) {
     case '+':
-      res = val1 + val2;
+      result = value1 + value2;
       break;
 
     case '-':
-      res = val1 - val2;
+      result = value1 - value2;
       break;
 
-    case '*':
-      res = val1 * val2;
+    case 'x':
+      result = value1 * value2;
       break;
 
     case '/':
-      res = val1 / val2;
+      result = value1 / value2;
       break;
   }
+  return result;
+}
 
+int main(int argc, char const *argv[]) {
+  double val1, val2, res;
+  char *ptr;
+  int arg = argc/2;
+  char op[arg];
+  double val[arg];
+  //Check args
+  /*for (int i=1; i< argc; i++) {
+    printf("arg%d=%s\n", i, argv[i]);
+  }*/
+
+  //Check number of args
+  if(argc%2==1){
+    printf("error : invalid number of arguments\n");
+    return(0);
+  }
+
+  //Check args
+  for(int i=1;i<argc;i++){
+    if(i%2==1){ //Check Number
+      ptr = "";
+      val[(i-1)/2] = strtod(argv[i], &ptr);
+      if(strlen(ptr)!=0){
+        printf("error : invald argument (%s)", argv[i]);
+        return 0;
+      }
+    } else{ //Check Operator
+      if(strlen(argv[i]) == 1){ //Check if the string has only one char
+        ptr = strchr("+-x/",argv[i][0]); //Check if operator is in calculator
+        if(ptr == NULL){
+          printf("error : invald operator (%s)", argv[i]);
+          return 0;
+        } else {
+            op[i/2] = argv[i][0];
+        }
+      } else { //Ope with multiple char
+        printf("error : invald operator (%s)", argv[i]);
+        return 0;
+      }
+    }
+  }
+  res = val[0];
+
+  //Check number of arg
+  //printf("Argc %d\n", arg);
+
+  //Calculate
+  for(int i=1;i<arg;i++){
+    res = calculate(res, val[i], op[i]);
+  }
   printf("%lf\n", res);
   return 0;
 }
