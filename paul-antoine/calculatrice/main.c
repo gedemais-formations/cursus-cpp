f5e1d8466fe481e0fb1eaccb44c2baec11867770
neokyr <p00riel.kyrillin@gmail.com>
2022-12-14 09:15:50 -0500

    Add verification of numbers

--- paul-antoine/calculatrice/main.c
+++ paul-antoine/calculatrice/main.c
@@ -8,9 +8,21 @@ int main(int argc, char** argv) {
         printf("error : invalid number of arguments");
         return 1;
     }
-    float arg1 = atof(argv[1]);
+    char* test = NULL;
+    float arg1 = strtof(argv[1], &test);
+    if(!test || *test) {
+        printf("error : %s is not a valid number \n", argv[1]);
+        return 1;
+    }
+
     char* operator = argv[2];
-    float arg2 = atof(argv[3]);
+
+    test = NULL;
+    float arg2 = strtof(argv[3], &test);
+    if(!test || *test) {
+        printf("error : %s is not a valid number \n", argv[3]);
+        return 1;
+    }
 
     if(strcmp("+", operator) == 0) {
         printf("%.2f + %.2f = %.2f \n", arg1, arg2, arg1 + arg2);
