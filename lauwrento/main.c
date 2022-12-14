913e146a2e32e083e8f6819579a72b16f335711a
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-14 15:41:10 +0100

    Calculatrice classique

---
+++ lauwrento/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Main function for calc
+
+int main(int argc, char **argv)
+{
+	if(argc != 4){
+		printf("error : invalid number of arguments\n");
+		return (2);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+
+	if(!strcmp(argv[2], "+")){
+		int res = num1 + num2;
+		printf("%d\n", res);
+	}else if(!strcmp(argv[2], "-")){
+		int res = num1 - num2;
+		printf("%d\n", res);
+	}else if(!strcmp(argv[2], "*")){
+		int res = num1 * num2;
+		printf("%d\n", res);
+	}else if(!strcmp(argv[2], "/")){
+		int res = num1 / num2;
+		printf("%d\n", res);
+	}else {
+		printf("error : invalid operator\n");
+		return (3);
+	}
+
+	return(0);
+}
