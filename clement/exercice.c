b463baadca1f275e081f2442a3e438a483b6120c
clem3806 <clement.cernesson.ad@gmail.com>
2022-12-14 14:07:15 +0000

    programme fonctionnel

--- clement/exercice.c
+++ clement/exercice.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main (int argc, char **argv){
-	int a = 5;
-	int b = 3;
-	char *o = "-";
-	if(strcmp(o, "+") == 0){
-		printf("%d", a+b);
-		printf("\n");
+	int a;
+	int b;
+	char *o;
+	if(sizeof(*argv) < 4 || atoi(argv[1]) == 0 || atoi(argv[3]) == 0){
+		printf("error : invalid number of arguments or invalid arguments\n");
 	}
 	else{
-		if(strcmp(o, "-") == 0){
-			printf("%d", a-b);
+		a = atoi(argv[1]);
+		o = argv[2];
+		b = atoi(argv[3]);
+		if(strcmp(o, "+") == 0){
+			printf("%d", a+b);
 			printf("\n");
 		}
 		else{
-			if(strcmp(o, "*") == 0){
-				printf("%d", a*b);
+			if(strcmp(o, "-") == 0){
+				printf("%d", a-b);
 				printf("\n");
 			}
 			else{
-				if(strcmp(o, "/") == 0){
-					printf("%d", a/b);
+				if(strcmp(o, "*") == 0){
+					printf("%d", a*b);
 					printf("\n");
 				}
 				else{
-					printf("Error: invalid operator.\n");
+					if(strcmp(o, "/") == 0){
+						printf("%d", a/b);
+						printf("\n");
+					}
+					else{
+						printf("Error: invalid operator.\n");
+					}
 				}
 			}
 		}
