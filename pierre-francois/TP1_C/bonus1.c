25e4524707d9025add826cc28da46bcf8b1387dd
pfdegorce <pfdegorce@outlook.com>
2022-12-15 13:37:58 +0100

    rajout de commentaires dans bonus 1 - sans essai

--- pierre-francois/TP1_C/bonus1.c
+++ pierre-francois/TP1_C/bonus1.c
@@ -4,8 +4,14 @@
 
 int main(int argc, char **argv)
 {
+//On récupère la longueur du tableau argv
 	int nombreArguments = argc;
+//On initialise la valeur du résultat avec le premier nombre rentré
 	float resultat = atof(argv[1]);
+
+/*On utilise une boucle for dont le corps est semblable à la fonction de
+  base. On initialise la boucle for avec le second nombre présent dans 
+  les arguments */
 
 	for (int i = 3; i <= nombreArgument-2; i = i+2)
 	{
