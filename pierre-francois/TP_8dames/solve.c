1d84ba537f153bd2dfed710d9be8583b356fb569
pfdegorce <pfdegorce@outlook.com>
2022-12-19 12:44:42 +0100

    ajout du template

---
+++ pierre-francois/TP_8dames/solve.c
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#define CHESS_SIZE 8
+
+bool check_queen(int chessboard[CHESS_SIZE][CHESS_SIZE], int x, int y)
+{
+    // check lines, column, diags
+    return (true);
+}
+
+void print_chessboard(int chessboard[CHESS_SIZE][CHESS_SIZE])
+{
+    printf("--------------------------\n");
+    for (int x = 0; x < CHESS_SIZE; x++)
+    {
+        for (int y = 0; y < CHESS_SIZE; y++)
+            printf("%d ", chessboard[x][y]);
+        printf("\n");
+    }
+    printf("--------------------------\n");
+}
+
+int solve(int chessboard[CHESS_SIZE][CHESS_SIZE], int x)
+{
+    (void)chessboard;
+    (void)x;
+    return (0);
+}
+
+int    main(void)
+{
+    int chessboard[CHESS_SIZE][CHESS_SIZE] = {};
+
+    print_chessboard(chessboard);
+    printf("%d\n", solve(chessboard, 0));
+}
