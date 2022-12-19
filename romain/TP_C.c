4b00007432fd8247d39cb38f98557409adb82c34
gedemais-formations <demaison.geoffrey@gmail.com>
2022-12-19 05:57:26 -0500

    corrected

--- romain/TP_C.c
+++ romain/TP_C.c
@@ -1,19 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int main (int argc, char **argv)
+
+void	calculatrice(int a, int b, char *c)
 {
-	//int calculatrice (){
-int a = atoi(argv[1]) ;
-int b = atoi(argv[3]) ;
-char * c = argv[2];
-int r ;
-//printf("écrire un premier numéro : \n");
-//scanf("%d",&a);
-//printf("écrire votre deuxième numéro : \n");
-//scanf("%d",&b);
-//printf("type d'opération : \n");
-//scanf ("%s",c);
+	float	r;
 
 	if(strcmp(c,"+")==0)
 	{r= a+b ;
@@ -31,7 +22,38 @@ int r ;
 	else{
 		printf("Erreur d'opérateur ");
 	}
-printf("%d \n",r);
-return 0 ;
+	printf("%f\n",r);
 }
-//}
+
+int		get_user_input(int argc, char **argv, int *a, int *b, char **c)
+{
+	if (argc != 4)
+	{
+		printf("Nombre de parametres invalide.\n");
+		return (1);
+	}
+	*a = atoi(argv[1]);
+	*b = atoi(argv[3]);
+	*c = argv[2];
+
+	return (0);
+}
+
+int		main (int argc, char **argv)
+{
+	//int calculatrice (){
+	int a, b;
+	char * c;
+
+	get_user_input(argc, argv, &a, &b, &c);
+	calculatrice(a, b, c);
+//printf("écrire un premier numéro : \n");
+//scanf("%d",&a);
+//printf("écrire votre deuxième numéro : \n");
+//scanf("%d",&b);
+//printf("type d'opération : \n");
+//scanf ("%s",c);
+
+
+	return (0);
+}
