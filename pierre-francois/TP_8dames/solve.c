6cd9a9150aea82fdd5c46e52ffc764c4f6383539
pfdegorce <pfdegorce@outlook.com>
2022-12-19 14:14:20 +0100

    ajout des premières lignes dans main

--- pierre-francois/TP_8dames/solve.c
+++ pierre-francois/TP_8dames/solve.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 #define CHESS_SIZE 8
 
 bool check_queen(int chessboard[CHESS_SIZE][CHESS_SIZE], int x, int y)
@@ -28,6 +29,9 @@ int solve(int chessboard[CHESS_SIZE][CHESS_SIZE], int x)
 
 int    main(void)
 {
+	for (unsigned int i = 0; i < CHESS_SIZE; i++)
+        memset(chessboard[i], 0, sizeof(int) * CHESS_SIZE);
+
     int chessboard[CHESS_SIZE][CHESS_SIZE] = {};
 
     print_chessboard(chessboard);
