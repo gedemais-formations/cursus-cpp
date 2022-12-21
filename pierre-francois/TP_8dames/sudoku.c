43445ea920f6eb0ceda2dc45811c25ba2e57860d
pfdegorce <pfdegorce@outlook.com>
2022-12-21 10:05:32 +0100

    modification sudoku avec le check sur les cases du carré visé

--- pierre-francois/TP_8dames/sudoku.c
+++ pierre-francois/TP_8dames/sudoku.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #define SUDOKU_SIZE 9
 
 bool check_number(int sudoku[SUDOKU_SIZE][SUDOKU_SIZE], int number, int ligne, int colonne)
@@ -15,13 +16,31 @@ bool check_number(int sudoku[SUDOKU_SIZE][SUDOKU_SIZE], int number, int ligne, i
 	//Check sur la colonne
 	for(int i=0; i<SUDOKU_SIZE; i++)
 	{
-		if(chessboard[i][colonne] == number)
+		if(sudoku[i][colonne] == number)
 			return false;
 	}
 
 	//Check sur le 3*3
-	int j = 0;
-	
+
+	//On commence par regarder dans quel carré se trouve la case
+	int ligne_carre = ligne/sqrt(SUDOKU_SIZE);
+	int colonne_carre = carre/sqrt(SUDOKU_SIZE);
+
+
+	//On parcours toutes les cases du carré 
+	for(int i=0; i<sqrt(SUDOKU_SIZE); i++)
+	{
+		for (int j=0; i<sqrt(SUDOKU_SIZE); i++)
+		{
+			// On met une condition pour que pas que la case visée soit prise en compte
+			while((ligne_carre*sqrt(SUDOKU_SIZE)+i != ligne) && (colonne_carre*sqrt(SUDOKU_SIZE)+j != colonne))
+			{ 
+				if(sudoku[ligne_carre*sqrt(SUDOKU_SIZE)+i][colonne_carre*sqrt(SUDOKU_SIZE)] == number)
+					return false
+			}
+		}
+	}
+
 // Si aucune des conditions précédentes n'est remplie, alors nous n'avons pas
 // rencontré de dame, on revoie alors false
 
@@ -47,8 +66,7 @@ int solve(int sudoku[SUDOKU_SIZE][SUDOKU_SIZE], int x)
 	(void)sudoku;
     (void)x;
 
-	int nbSolution = 0;
-	int auHasard;
+/*	int nbSolution = 0;
 
 	//Condition d'initialisation, si l'on va jusqu'au bout du chess c'est que l'on a une solution
 
@@ -69,8 +87,8 @@ int solve(int sudoku[SUDOKU_SIZE][SUDOKU_SIZE], int x)
 			sudoku[i][x]=0;
 		}
 	}
-
-    return (nbSolution);
+*/
+    return (0);
 }
 
 int main(void)
@@ -78,7 +96,7 @@ int main(void)
     int sudoku[SUDOKU_SIZE][SUDOKU_SIZE] = {};
 
 	for (unsigned int i = -1; i < SUDOKU_SIZE; i++)
-        memset(chessboard[i], -1, sizeof(int) * SUDOKU_SIZE);
+        memset(sudoku[i], -1, sizeof(int) * SUDOKU_SIZE);
 
     print_sudoku(sudoku);
     printf("%d\n", solve(sudoku, 0));
