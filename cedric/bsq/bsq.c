2720dc1064fac4b047aadda6887537c520b88137
cedric <c.darrou@gmx.com>
2023-01-02 10:07:29 +0000

    Read a file (BSQ)

--- cedric/bsq/bsq.c
+++ cedric/bsq/bsq.c
@@ -5,20 +5,23 @@
 #include<errno.h>
 #include<string.h>
 
-const char * open_read(char * file_name, int byte){
+const char * open_read(char *file_name, int byte){
     int fd, sz;
-    char *c = "";
+    char *c = (char *) calloc(100, sizeof(char));
     fd = open(file_name, O_RDONLY);
-    if(fd < 0){
-      printf("Erreur, ouverture du fichier impossible\n");
-      exit(1);
-    }
+    //printf("%s\n", file_name);
     //Open file
+    if(fd < 0){
+      printf("Error, cannot open the file : %s\n", strerror(errno));
+      return("Cannot open file");
+    }
+    //Read file
     sz = read(fd, c, byte);
     if(sz == -1){
-      printf("Cannot open file : %s\n", strerror(errno));
+      printf("Error, cannot read file : %s\n", strerror(errno));
+      return("File empty");
     }
-    printf("Contenu du fichier c : %s \n", c);
+    //printf("Contenu du fichier c : %s \n", c);
 
     return (c);
 }
@@ -27,6 +30,8 @@ const char * open_read(char * file_name, int byte){
 int main() {
   //USE GETCWD to get print
   //Path : home/cedric/cursus-cpp/cedric/
-  open_read("foo.txt", 100);
+  printf("%s\n", open_read("foo.txt", 100));
   return 0;
 }
+
+// gcc -Wall -Werror -Wextra -g3 -fsanitize=address bsq.c -o bsq
