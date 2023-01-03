5ec913d7c1a90b310e3fa980012bc610fd09fe70
jbaptistec <champilou.jbaptiste@gmail.com>
2023-01-03 06:52:50 -0500

    [bsq.c] définition des grandes étapes de construction du futur code par JBC

---
+++ JB/bsq/bsq.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+
+void main (){
+
+// 0: verifier que la taille du fichier peut être lu
+//
+// 1: lire le/les fichier(s) transmis
+// l'adresse du (des) fichier(s) est/sont spécifié(s) dans les arguments de la fonction main
+// 
+// 2: vérification du fichier:
+// 	a: vérifier qu'il y a un retour à la ligne à la fin de chaque ligne
+// 	>> renvoyer un message erreur spécifique si non
+// 	b: vérifier qu'il y a au moins une ligne d'une case
+// 	>> renvoyer un message erreur spécifique si non
+// 	c: vérifier que toutes les lignes (sauf la première) ont la même longueur
+//	>> renvoyer un message erreur spécifique si non
+//	d: vérifier que les caractères de la grille sont ceux de la première ligne
+//	>> renvoyer un message erreur spécifique si non
+//
+// 3: trouver le plus grand carré de caractères vides
+// 4: si plusieurs solutions, définir le carré à remplir
+// 5: remplir le carré sélectionné de caractères pleins
+
+
+return (0)
+}
