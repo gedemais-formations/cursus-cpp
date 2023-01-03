f513216ae074497ccd74669afc44aab7717edf0c
cedric <c.darrou@gmx.com>
2023-01-03 17:05:49 +0100

    end of day

--- cedric/bsq/bsq.c
+++ cedric/bsq/bsq.c
@@ -5,10 +5,9 @@
 #include<errno.h>
 #include<string.h>
 #include <sys/stat.h>
+#include <math.h>
 
-
-#define SIZE 100
-
+// Open a file, read it, close it and return the file content
 char *open_read(char *file_name, int buffer_size){
   int fd, sz, cl;
   char *buffer;
@@ -49,6 +48,7 @@ char *open_read(char *file_name, int buffer_size){
   return (buffer);
 } 
 
+// Count elements on the first line
 int count_char_per_line(char *file_content){
   int count = 0;
   while(file_content[count] != '\n'){
@@ -57,7 +57,85 @@ int count_char_per_line(char *file_content){
   return count;
 }
 
+int count_lines(char *file_content){
+  int count = 0;
+  int i = 0;
+  while(file_content[i] != '\0'){
+    if(file_content[i] == '\n'){
+      count ++;
+    }
+    i++;
+  }
+  return (count);
+}
 
+// Find an o on a cell
+int find_an_o(char **tab, int x, int y){
+  if(tab[x][y]=='o'){
+    return (1);
+  } else {
+    return (0);
+  }
+}
+
+// Print tab
+int print_tab(char **tab){
+  int i = 0;
+  int length = sizeof tab;
+  int row = sizeof(tab) / sizeof(&tab[0]);
+  while(i < length){
+    printf("%c", tab[i/row][i%row]);
+    if(i%length==length-1){
+      printf("\n");
+    }
+    i++;
+  }
+  return (0);
+}
+
+// Try to find a square
+int find_square(char **tab, char size_square){
+  int find = 1;
+  int i = 0;
+  while(i < size_square && find == 1){
+    if(find_an_o(tab, i/size_square, i%size_square)==1){
+      find = 0;
+      return 0;
+    } else {
+      tab[i/size_square][i/size_square]='x';
+    }
+  }
+  print_tab(tab);
+  return (1);
+}
+
+int solve(char *file_content, int count_line, int count_row){
+  char **tab = NULL;
+  int i = 0;
+  tab = (char **) malloc(sizeof(char*) * count_row);
+  for(i = 0; i < count_line; i++){
+    tab[i] = (char *) malloc(sizeof(char) * count_line);
+  }
+  
+  //Transform into a tab
+  while(file_content[i] != '\0'){
+    tab[i/count_row][i%count_row] = file_content[i];
+    i++;
+  }
+  
+  //Find a solution
+  i = count_line;
+  while(find_square(&tab[0], i) == 0 && i < 1){
+    i--;
+  }
+
+  for(i = 0; i < count_line; i++){
+    free(tab[i]);
+  }
+  free(tab);
+
+  return (0);
+}
 
 // int argc, char const *argv[]
 int main() {
@@ -77,11 +155,16 @@ int main() {
 
   file_content = open_read(file_name, sb.st_size);
   if(!file_content){
-    printf("%d", count_char_per_line(file_content));
+    return (-1);
+  } else {
+    //printf("%d\n", count_char_per_line(file_content));
+    printf("Check 1");
+    fflush(stdout);
+    solve(file_content, count_lines(file_content), 
+      count_char_per_line(file_content));
+    free(file_content);
+    return 0;
   }
-  
-  free(file_content);
-  return 0;
 }
 
-// gcc -Wall -Werror -Wextra -g3 -fsanitize=address bsq.c -o bsq
+// gcc -Wall -Werror -Wextra -g3 -fsanitize=address bsq.c -o bsq -lm
