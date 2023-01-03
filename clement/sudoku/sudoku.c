bf8ad50db9cd6175af5409a16c183d680244e50d
clem3806 <clement.cernesson.ad@gmail.com>
2023-01-03 14:12:29 +0000

    génération de la structure de vérification de la grille

--- clement/sudoku/sudoku.c
+++ clement/sudoku/sudoku.c
@@ -1,6 +1,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+//énoncé : avec une grille donnée, trouver toutes les solutions possibles
+
 void print_sudoku(int grille[9][9])
 {
     printf("--------------------------\n");
@@ -23,9 +25,66 @@ void print_sudoku(int grille[9][9])
     }
 }
 
-int    main(void)
+bool check_sudoku(int grille[9][9], int x, int y, int z)
 {
-    int grille[9][9] = {};
+    return true;
+}
+
+int solve(int grille[9][9], int x)
+{
+    int soluce = 0;
+    if( x == 8)
+    {
+        for(int y = 0; y < 9; y++)
+        {
+            for(int z = 1; z <= 9; z++)
+            {
+                if(check_sudoku(grille, x, y, z))
+                {
+                    grille[x][y] = 1;
+                    printf("solution trouvée\n");
+                    print_sudoku(grille);
+                    soluce++;
+                    grille[x][y] = 0;
+                }
+            }
+        }
+    }
+    else
+    {
+        for(int y = 0; y < 9; y++){
+            for(int z = 1; z <= 9; z++)
+            {
+                if(check_sudoku(grille, x, y, z)){
+                    grille[x][y] = 1;
+                    soluce += solve(grille, x+1);
+                    grille[x][y] = 0;
+                }
+            }
+        }
+    }
+    //todo: ajouter les reines quand elles sont valides et remettre le tableau à son état précédent quand on atteint une impasse.
+    return (soluce);
+}
+
+int main(void)
+{
+    int grille[9][9] = {
+        {0,0,0, 0,0,0, 0,0,0},
+        {0,0,0, 0,0,0, 0,0,0},
+        {0,0,0, 0,0,0, 0,0,0},
+        
+        {0,0,0, 0,0,0, 0,0,0},
+        {0,0,0, 0,0,0, 0,0,0},
+        {0,0,0, 0,0,0, 0,0,0},
+        
+        {0,0,0, 0,0,0, 0,0,0},
+        {0,0,0, 0,0,0, 0,0,0},
+        {0,0,0, 0,0,0, 0,0,0}
+    };
     
+    printf("grille de départ\n");
     print_sudoku(grille);
+    
+    //printf("Il y a %d solutions possibles.\n", solve(grille[9][9], 0));
 }
