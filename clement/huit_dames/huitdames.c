20f5bd52c814dbe388665fc5859ed0f5ef6fd7ce
clem3806 <clement.cernesson.ad@gmail.com>
2023-01-03 13:48:30 +0000

    resolution du problème avec huitdames, apparemment le "if(x+1==CHESS_SIZE)" devait absolument être la première boucle

--- clement/huit_dames/huitdames.c
+++ clement/huit_dames/huitdames.c
@@ -4,15 +4,16 @@
 
 bool check_queen(int chessboard[CHESS_SIZE][CHESS_SIZE], int x, int y)
 {
-    int i = 1;
+    int i = 0;
+    
     //droite
-    while((y+i)<=CHESS_SIZE){
+    while((y+i)<CHESS_SIZE){
         if(chessboard[x][y+i] == 1){
             return (false);
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //gauche
     while((y-i)>=0){
         if(chessboard[x][y-i] == 1){
@@ -20,15 +21,15 @@ bool check_queen(int chessboard[CHESS_SIZE][CHESS_SIZE], int x, int y)
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //bas
-    while((x+i)<=CHESS_SIZE){
+    while((x+i)<CHESS_SIZE){
         if(chessboard[x+i][y] == 1){
             return (false);
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //haut
     while((x-i)>=0){
         if(chessboard[x-i][y] == 1){
@@ -36,31 +37,31 @@ bool check_queen(int chessboard[CHESS_SIZE][CHESS_SIZE], int x, int y)
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //bas droite
-    while((x+i)<=CHESS_SIZE && (y+i)<=CHESS_SIZE){
+    while((x+i)<CHESS_SIZE && (y+i)<CHESS_SIZE){
         if(chessboard[x+i][y+i] == 1){
             return (false);
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //bas gauche
-    while((x-i)>=0 && (y+i)<=CHESS_SIZE){
+    while((x-i)>=0 && (y+i)<CHESS_SIZE){
         if(chessboard[x-i][y+i] == 1){
             return (false);
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //haut droite
-    while((x+i)<=CHESS_SIZE && (y-i)>=0){
+    while((x+i)<CHESS_SIZE && (y-i)>=0){
         if(chessboard[x+i][y-i] == 1){
             return (false);
         }
         i++;
     }
-    i = 1;
+    i = 0;
     //haut gauche
     while((x-i)>=0 && (y-i)>=0){
         if(chessboard[x-i][y-i] == 1){
@@ -68,7 +69,7 @@ bool check_queen(int chessboard[CHESS_SIZE][CHESS_SIZE], int x, int y)
         }
         i++;
     }
-    // check lines, column, diags
+    // check lines, column, diags    
     return (true);
 }
 
@@ -86,17 +87,35 @@ void print_chessboard(int chessboard[CHESS_SIZE][CHESS_SIZE])
 
 int solve(int chessboard[CHESS_SIZE][CHESS_SIZE], int x)
 {
+    //print_chessboard(chessboard);
+    //getc(stdin);
     int possibilites = 0;
-    for(int y = 0; y < CHESS_SIZE; y++){
-        if(check_queen(chessboard, x, y){
-            if(x+1 == CHESS_SIZE){
-            	return(1);
-            }
-            else{
-                possibilites += solve(chessboard, x+1);
+    if( x+1 == CHESS_SIZE){
+        for(int y = 0; y < CHESS_SIZE; y++){
+            if(check_queen(chessboard, x, y)){
+                //printf("%d\n", check_queen(chessboard, y, x));
+                //printf("%d ", x);
+                //print_chessboard(chessboard);
+                chessboard[x][y] = 1;
+                printf("solution trouvée\n");
+                print_chessboard(chessboard);
+                possibilites++;
+                chessboard[x][y] = 0;
             }
         }
     }
+    else{
+        for(int y = 0; y < CHESS_SIZE; y++){
+            if(check_queen(chessboard, x, y)){
+            	chessboard[x][y] = 1;
+            	//printf("%d ", chessboard[y][x]);
+                //print_chessboard(chessboard);
+                possibilites += solve(chessboard, x+1);
+                //printf("poss = %d\n", possibilites);
+                chessboard[x][y] = 0;
+                }
+            }
+        }
     //todo: ajouter les reines quand elles sont valides et remettre le tableau à son état précédent quand on atteint une impasse.
     return (possibilites);
 }
@@ -104,7 +123,13 @@ int solve(int chessboard[CHESS_SIZE][CHESS_SIZE], int x)
 int    main(void)
 {
     int chessboard[CHESS_SIZE][CHESS_SIZE] = {};
-    
+    /*chessboard[0][0] = 1;
+    chessboard[5][1] = 1;
+    chessboard[7][2] = 1;
+    chessboard[2][3] = 1;
+    chessboard[6][4] = 1;
+    chessboard[3][5] = 1;
+    chessboard[1][6] = 1;*/
     print_chessboard(chessboard);
     printf("%d\n", solve(chessboard, 0));
 }
