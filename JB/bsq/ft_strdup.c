a3782f1ba08efcd4db311cf352a4510d9ed029e3
jbaptistec <champilou.jbaptiste@gmail.com>
2023-01-04 08:05:10 -0500

    [ft_strdup] code qui compile non testé hiphiphip ouwa par JBC

--- JB/bsq/ft_strdup.c
+++ JB/bsq/ft_strdup.c
@@ -8,14 +8,17 @@
 #include<stdbool.h>
 // pour utiliser READ
 #include<unistd.h>
-
+// pour utiliser OPEN
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
 int main(int argc, char **argv){
 	// vérification qu'il y a uniquement 2 arguments dans la fonction, la chaine de caractère et le(s) charset
 	// dans un premier temps il n'y aura qu'un seul char défini dans charset (1)
 	if (argc != 3){
-		print("Invalid number of arguments for ft_strdup! \n");
-		return (false);
+		printf("Invalid number of arguments for ft_strdup! \n");
+		return(false);
 	}
 
 	// initialisation d'un compteur qui va définir la taille du premier élément pour définir l'espace mémoire à allouer
@@ -25,26 +28,36 @@ int main(int argc, char **argv){
 	char elt_char = argv[1][i];
 	
 	//(1)
-	while (elt_char != arg[2]){
-		if (elt_char == "\0"){
-			print("No charset found");
-			return (false);	
+	while (elt_char != *argv[2]){
+		if (elt_char == '\0'){
+			printf("No charset found \n");
+			return(false);	
 		}
 		else {
 		i++;
 		elt_length++;
 		}
-		return (true);
+		return(true);
 	}
 	
+	// récupération du file descriptor de argv[1] grace à la fct OPEN
+	int fd_argv1 = open(argv[1], O_RDONLY);
+		if (fd_argv1 == -1){
+		printf("No file descriptor for argv[1] \n");
+		return(false);
+		}
+
 	// allocation d'un espace mémoire de taille elt_length avec malloc
 	// récupération de l'adresse de cet espace en définissant la variable elt_mem
-	void elt_mem = void *malloc(size_t elt_length);
+	char * elt_mem = (char *) malloc(elt_length);
 	
 	// copie les elt_length-1 premier char dans l'espace alloué
-	ssize_t read(argv[1], *elt_mem, (elt_length - 1));
+	read(fd_argv1, elt_mem, (elt_length - 1));
 	
 	// vérification que cela à bien ecrit à l'adresse dédié
-	printf("%c\n", *elt_mem);
+	printf("%s\n", elt_mem);
+
+	// libération de l'espace mémoire
+	free(elt_mem);
 }
 
