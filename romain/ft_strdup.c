88501b6fb37deff26acf2dab88c3853b462e39a0
romainzap <romain.zap@gmail.com>
2023-01-05 11:49:25 +0100

    strdup marche un peu

--- romain/ft_strdup.c
+++ romain/ft_strdup.c
@@ -2,27 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+int recherche(char lettre, char *phrase)
+{
+	int longue = strlen(phrase);
+		for (int i = 0 ;i<longue ; i++)
+		{
+			if(lettre ==phrase[i])
+			{ 
+			   return 1 ;
+			}
+		}
+return 0 ;
+}
+
 
 
 char *ft_strdup(char *s, char *charset)
 {
  int longueur = 0;
-    int i = 0;
-char *test = 0 ; 
-   longueur = strlen(s);
-    test = malloc(sizeof(char)*longueur);
-
-    while (s[i]==*test)
+ int i = 0;
+ char *test = 0 ; 
+   while (recherche(s[longueur],charset)==0)
     {
-        (charset)[i] = s[i];
+        longueur++;
+    }
+
+   test = malloc(sizeof(char)*(longueur+1));
+  while (recherche(s[i],charset)==0)
+    {
+        test[i] = s[i];
         i++;
     }
-    (charset)[i] = '\0';
-    return charset;
+
+    test[i] = '\0';
+
+    return test;
 }
 
 int main ()
 {
-	ft_strdup("Projet BLUE LOCK","o");
+	char *resultat = ft_strdup("Bonjour tout le monde","o");
+	printf("%s",resultat);
+	free(resultat);
 	return 0 ;
 }
