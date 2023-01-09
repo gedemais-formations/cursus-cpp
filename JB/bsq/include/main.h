946af32e2f9e4e8045b23a334fd1548989f699d1
jbaptistec <champilou.jbaptiste@gmail.com>
2023-01-09 10:31:11 -0500

    [get_metadata.c] premier commit de get_metadata.c untest ans uncompile by JBC

--- JB/bsq/include/main.h
+++ JB/bsq/include/main.h
@@ -4,6 +4,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct s_metadata{
+         int nb_line;
+         int nb_col;
+         char empty_char;
+         char obstacle_char;
+         char full_char;
+	 char** board;	
+}        metadata_t;
+
 // inclure tous les prototypes des fonctions qui seront utilisés
 int char_charset(char string_char_x,char *charset);
 char *ft_strdup (char *string_char, char *charset);
---
+++ JB/bsq/src/get_metadata.c
@@ -0,0 +1,151 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+// pour utiliser la fonction malloc
+#include<stdlib.h>
+//pour utiliser la fonction open et stat
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include"main.h"
+
+// Pour chaque argument de la fonction main
+// fonction get_map
+// une fraction qui récupère les metadata (1ere ligne du fichier d'entrée)
+// 1: ouvrir le/les fichier(s) transmis
+// utilisation de la fonction open
+
+
+
+// char get_map(char*ad_file_map){}
+
+int get_metadata(char* map_init, metadata_t* m_map){
+
+	char* first_line = ft_strdup(map_init, "\n");
+	if (first_line == NULL){
+		printf("Memory allocation trouble");
+		return(1);
+	}
+	int length_fl = strlen(first_line);
+	if (length_fl <= 4){	// nb minimal de caractère à define en tête de fichier
+		print("Not enough arguments in line 1");
+		return(2);
+	}
+	m_map.full_char = first_line[length_fl - 1];
+	m_map.obstacle_char = first_line[length_fl - 2];
+	m_map.empty_char = first_line[length_fl - 3];
+	
+	if(m_map.full_char == m_map.obstacle_char 
+		|| m_map.full_char == m_map.empty_char 
+		|| m_map.obstacle_char != m_map.empty_char){
+		print("The three characteres are not unique");
+		return(3);
+	}
+	
+	first_line[length_fl - 3] = '\0'
+	m_map.nb_line = atoi(map_init);
+	printf("Numbre of line is : %d \n", m_map.nb_line);
+	
+	char** for_second_line = ft_split(map_init, "\n");
+	if (for_second_line == NULL){
+		printf("Memory allocation trouble 2");
+		return(4);
+	}
+	m_map.nb_col = strlen(for_second_line[1]);		
+}	
+	
+
+// void get_board(){}
+
+
+char* read_map(char *file_map){					// file_map est une variable qui contient l'adresse du fichier
+	int fd_open_file = open(file_map, O_RDONLY);		// variable open_file est le fd (file descriptor) de file_map
+	// vérification que l'on peut ouvrir et sinon renvoyé une erreur
+	if (open_file < 0){
+		printf("File failed to open. \n");
+		return(NULL);
+	}
+	
+
+	// obtenir la taille du fichier à malloc
+	// déclare une variable structure stat > vide qui sera entré en paramètre de stat
+	struct stat buf_stat;
+	// on rempli cette variable avec la fonction stat, passage de variable par référence
+	// et vérification que stat a bien fonctionné
+	if(stat(file_map, &buf_stat) == -1){
+		perror(strerror(errno));
+		perror("Cannot acces stat file\n");
+	}
+	
+	int file_size = buf_stat.st_size;
+	char* map_init = NULL;
+	map_init = (char*)malloc(sizeof(char) * (file_size + 1));
+	if (!map_init){
+		printf("No more memory available");
+		return(NULL);
+	}
+
+	// copie les file_size premier char dans l'espace alloué avec read
+	// et stocké dans re_file_map le nombre d'octet lus
+        int rd_file_map = read(fd_open_file, map_init, file_size);
+        
+        // ajout de \O en position rd_elt_mem (fin de chaine) soit elt_length
+        // normalement
+        // > pour signifier la fin de la chaine de caractère (cc)
+        // caractère obligatoire sinon continuera à lire après la fin de la cc
+        map_init[rd_file_map] = '\0';
+
+        // vérification que cela à bien ecrit à l'adresse dédié
+        printf("%s\n", map_init);
+	
+        // libération de l'espace mémoire
+        free(map_init);
+
+	
+}
+	
+	
+// 2: lire la première ligne du/des fichier(s) transmis
+// l'adresse du (des) fichier(s) est/sont spécifié(s) dans les arguments de la fonction main
+// utilisation de la fonction read
+// il faut récupérer la première ligne jusqu'au premier \n >> utiliser ft_split()
+
+
+
+// 	a: vérifier que les caractères de la grille sont ceux de la première ligne
+//	>> renvoyer un message erreur spécifique si non
+//
+// 	b: vérifier qu'il y a au moins une ligne d'une case
+// 	Il doit y avoir au moins 2 lignes dans le fichier ET la seconde ligne doit avoir au moins un caractère
+// 	>> renvoyer un message erreur spécifique si non
+// 	
+// 	c: vérifier que toutes les lignes (sauf la première) ont la même longueur
+//	pour toutes les lignes l de l'index 1 à (last = dimension du fichier), size(
+//	>> renvoyer un message erreur spécifique si non
+// 	
+// 	d: vérifier qu'il y a un retour à la ligne à la fin de chaque ligne
+// 	>> renvoyer un message erreur spécifique si non
+//
+//une seconde fraction pour récupérer le board
+// 3: recupérer le board
+// !!: verifier la taille du fichier 
+// >> allouer un espace mémoire suffisant avec malloc pour la fonction
+// 
+// fonction solve
+// 4: trouver le plus grand carré de caractères vides
+// 
+// 5: si plusieurs solutions, définir le carré à remplir
+// 
+// 6: remplir le carré sélectionné de caractères pleins
+//
+// fonction free
+// 7: libérer la mémoire
+
+// int bsq(argc, argv)
+/*
+int main(argc, argv){
+return (0);
+}
+*/
