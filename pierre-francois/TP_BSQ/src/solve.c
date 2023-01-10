c684c5e99eb99c0c137fd66ad6ae80051b6bbc5a
pfdegorce <pfdegorce@outlook.com>
2023-01-10 13:04:29 +0100

    function solve modified

--- pierre-francois/TP_BSQ/src/solve.c
+++ pierre-francois/TP_BSQ/src/solve.c
@@ -1,13 +1,14 @@
 #include<main.h>
 
-int solve(char **tab)
+int solve(char **tab, map_t map)
 {
 
 	for(int i=0; tab[i] != NULL; i++)
 	{
 		for(int j=0; tab[i][j]!='\0'; i++)
 		{
-			
+			if(tab[i][j]==map->empty_char)
+
 		}
 	}
 	return 0;
