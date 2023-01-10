ff128753047fdba95d62ce7f97c25d02dda93cf4
jbaptistec <champilou.jbaptiste@gmail.com>
2023-01-10 14:31:17 -0500

    [solve.c] This close to finish, just the printing that do not work by JBC

--- JB/bsq/src/solve.c
+++ JB/bsq/src/solve.c
@@ -20,21 +20,31 @@ bool check (t_metadata* m_map, int start_x, int start_y, int sqr_side){
 	return (true);		
 }
 
+void write_full_char(t_metadata* m_map, int start_x, int start_y, int sqr_side){
+	for (int n = 0; n < start_x + sqr_side; n++){
+		for (int o = 0; o < start_y + sqr_side; o++){
+			m_map->board[n][o] = m_map->full_char;
+		}
+	}
+}
+
 int solve(t_metadata* m_map){
 	int sqr1_size = min_val(m_map->nb_line, m_map->nb_col);
-		for (int i = 0; i < sqr1_size; i++){
-			int sqr_side = sqr1_size - i;
-			int end_x = m_map->nb_col - sqr_side;
-			int end_y = m_map->nb_line - sqr_side;
-			for (int j = 0; j < end_x; j++){
-				for(int k = 0; k < end_y; k++){
-					if(check(m_map, j, k, sqr_side)){
-						printf("IT WORKS \n");
-						printf("Square start at %d %d and its side is %d \n", k, j, sqr_side);
-						exit(0);
-					}
+	for (int i = 0; i < sqr1_size; i++){
+		int sqr_side = sqr1_size - i;
+		int end_x = m_map->nb_col - sqr_side;
+		int end_y = m_map->nb_line - sqr_side;
+		for (int j = 0; j < end_x; j++){
+			for(int k = 0; k < end_y; k++){
+				if(check(m_map, j, k, sqr_side)){
+					write_full_char(m_map, j, k, sqr_side);
+					printf("IT WORKS \n");
+					printf("Square start at %d:%d and its side is %d \n", k, j, sqr_side);
+					printf("%s\n", *m_map->board);
+					exit(0);
 				}
 			}
 		}
+	}
 	return(1);
 }
